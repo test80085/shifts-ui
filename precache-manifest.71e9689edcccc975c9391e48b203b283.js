ff40f0a7b474c423b59d9caed982855d3fcabc52
root <root@localhost.localdomain>
2020-10-24 06:10:32 +0300

    Updates

---
+++ precache-manifest.71e9689edcccc975c9391e48b203b283.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fae5f70855e65f545eb6915b8af687",
+    "url": "/shifts-ui/index.html"
+  },
+  {
+    "revision": "7693e52dda4f40003b79",
+    "url": "/shifts-ui/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "103bacdbbb6ba0d28274",
+    "url": "/shifts-ui/static/css/main.120a8c3b.chunk.css"
+  },
+  {
+    "revision": "7693e52dda4f40003b79",
+    "url": "/shifts-ui/static/js/2.056b0bc0.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/shifts-ui/static/js/2.056b0bc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103bacdbbb6ba0d28274",
+    "url": "/shifts-ui/static/js/main.99c66ff0.chunk.js"
+  },
+  {
+    "revision": "acecde76cdee457b0473",
+    "url": "/shifts-ui/static/js/runtime-main.2993d9a5.js"
+  }
+]);
